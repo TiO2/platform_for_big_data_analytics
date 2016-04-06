b191e217f709b9c605fb6e5a0eb64ec1fe630959
pvichar <pvichar@ncsu.edu>
2016-04-06 11:49:52 -0400

    api calls for auto spin up of VMs
    - auto spin up of vms using Transcirrus apis
    - constants added to a single file which are the metadata for a
    hortonworks VM
    - initial calling function to test the apis

---
+++ auto_spin_up_api_calls.py
@@ -0,0 +1,139 @@
+#Methods to make REST API-calls to Transcirrus Box
+import constants
+import requests
+import json
+
+
+#Create a New Instance Specification
+def create_new_instance_specification(cpus, disk, name, ram):
+
+    try: 
+
+        request_url = base_url + project_id + '/instance_specifications'
+        data = "{\"cpus\":" + str(cpus) + "," + \
+               "\"disk\":" + str(disk) + "," + \
+               "\"name\":\"" + name + "\"," + \
+               "\"ram\":" + str(ram) + "," + \
+               "\"visibility\": \"private\"}"
+
+        print data + '\n'
+        response = requests.post(request_url, headers = headers, data = data)
+        print response.text + '\n'
+        r = response.json()
+        instance_specification_id = r['instance_specification']['id']
+        return instance_specification_id
+
+    #log the errors
+    except:
+        print 'Error ! Unexpected value returned'
+        
+        
+
+#Launch a New Sandbox Instance
+def launch_new_instance(instance_name, instance_specification_id):
+
+    try:
+        
+        request_url = base_url + project_id + '/instances'
+        data = "{\"image_id\":\"" + hortonworks_image_id + "\"," + \
+               "\"is_boot_from_volume\": false," + \
+               "\"name\":\"" + instance_name + "\"," + \
+               "\"network_name\":\"" + network_id + "\"," + \
+               "\"security_group_name\":\"" + security_group + "\"," + \
+               "\"security_key_name\":\"" + security_key + "\"," + \
+               "\"specification_id\":\"" + str(instance_specification_id) + "\"," + \
+               "\"zone\": \"nova\" }"
+        print data + '\n'
+        response = requests.post(request_url, headers = headers, data = data)
+        print response.text + '\n'
+        r = response.json()
+
+        #complete this later
+        instance_id = r[][]
+        return instance_id
+
+    except:
+        print 'Error ! Unexpected value returned'
+
+
+
+#Create a New Floating IP
+def create_new_floating_ip():
+
+    try:
+        
+        request_url = base_url + '/floating_ips'
+
+        #add network_id here
+        data = "{\"network_id\":\"" + network_id + "\"," + \
+               "\"project_id\":\"" + project_id + "\"}"
+        print data + '\n'
+        response = requests.post(request_url, headers = headers, data = data)
+        print response.text + '\n'
+        r = response.json()
+
+        #complete this later
+        floating_ip_id = r[][]
+        return floating_ip_id
+
+    except:
+        print 'Error ! Unexpected value returned'
+
+
+
+#Add the floating IP to the newly created instance
+def add_floating_ip_to_instance(instance_id, floating_ip_id):
+
+    try:
+
+        request_url = base_url + '/floating_ips' + floating_ip_id + '/action'
+
+        #add network_id here
+        data = "{\"action\": \"add\"," + \
+               "\"instance_id\":\"" + instance_id + "\"," + \
+               "\"project_id\":\"" + project_id + "\" }"
+        
+        print data + '\n'
+        response = requests.post(request_url, headers = headers, data = data)
+        print response.text + '\n'
+        r = response.json()
+
+        #return floating ip address to calling program
+        floating_ip_addr = r[][]
+        return floating_ip_addr
+        
+
+    except:
+        print 'Error ! Unexpected value returned'
+
+
+
+#Create a new spindle volume for the VM (limit to spindle for now)
+def create_new_spindle_volume(volume_name, volume_size, volume_type):
+
+    try:
+
+        if (volume_type = 'ssd'):
+            volume_id = total_ssd_volume_id
+        else:
+            if (volume_type = 'spindle'):
+            volume_id = total_spindle_volume_id
+            else:
+                print 'Error in type of volume field
+
+        request_url = base_url + project_id + '/volumes'
+
+        data = "{\"name\":\"" + volume_name + "\"," + \
+               "\"size\":" + volume_size + "," + \
+               "\"type\":\"" + volume_type + "\"," + \
+               "\"volume_id\":\"" + volume_id + "\"," + \
+               "\"zone\": \"nova\"}"
+        
+        print data + '\n'
+        response = requests.post(request_url, headers = headers, data = data)
+        print response.text + '\n'
+        r = response.json()
+
+    except:
+        print 'Error ! Unexpected value returned'
+
---
+++ constants.py
@@ -0,0 +1,18 @@
+#Constants to be used at various places
+#Includes specific id numbers of project, images, network, security group,
+#security keys, volumes
+#the url to call the transcirrus apis, headers to pass
+
+base_url = 'http://10.23.1.2:6969/v1.0/'
+headers = {'Accept' : 'application/json' , 'username' : 'admin_team7' , 'password' : 'team7rocks'}
+project_id = '2aad80ab68d041649d1823ce024c85de'
+hortonworks_image_id = '644d6b91-c831-43f1-80ab-976e38fad506'
+network_name = 'network_team7'
+network_id = ''
+security_group_name = 'secgroup_team7'
+security_group_id = ''
+security_key_name = 'seckey_team7_6761'
+security_key_id = ''
+total_spindle_volume_id = ''
+total_ssd_volume_id = ''
+
