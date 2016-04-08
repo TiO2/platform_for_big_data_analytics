70e8934d7341b1b6cfbd70308c2044a6b5ed9c1c
pvichar <pvichar@ncsu.edu>
2016-04-07 21:12:26 -0400

    Auto Spin Up - Floating IP attached to VM
    - Launches an instance
    - creates a floating ip
    - attaches the floating ip
    - returns floating ip to use

--- auto_spin_up_api_calls.py
+++ auto_spin_up_api_calls.py
@@ -1,36 +1,36 @@
 #Methods to make REST API-calls to Transcirrus Box
-import constants
+from constants import *
 import requests
 import json
 
-
-#Create a New Instance Specification
-def create_new_instance_specification(cpus, disk, name, ram):
-
-    try: 
-
-        request_url = base_url + project_id + '/instance_specifications'
-        data = "{\"cpus\":" + str(cpus) + "," + \
-               "\"disk\":" + str(disk) + "," + \
-               "\"name\":\"" + name + "\"," + \
-               "\"ram\":" + str(ram) + "," + \
-               "\"visibility\": \"private\"}"
-
-        print data + '\n'
-        response = requests.post(request_url, headers = headers, data = data)
-        print response.text + '\n'
-        r = response.json()
-        instance_specification_id = r['instance_specification']['id']
-        return instance_specification_id
-
-    #log the errors
-    except:
-        print 'Error ! Unexpected value returned'
-        
+##
+###Create a New Instance Specification
+##def create_new_instance_specification(cpus, disk, name, ram):
+##
+##    try: 
+##
+##        request_url = base_url + project_id + '/instance_specifications'
+##        data = "{\"cpus\":" + str(cpus) + "," + \
+##               "\"disk\":" + str(disk) + "," + \
+##               "\"name\":\"" + name + "\"," + \
+##               "\"ram\":" + str(ram) + "," + \
+##               "\"visibility\": \"private\"}"
+##
+##        print data + '\n'
+##        response = requests.post(request_url, headers = headers, data = data)
+##        print response.text + '\n'
+##        r = response.json()
+##        instance_specification_id = r['instance_specification']['id']
+##        return instance_specification_id
+##
+##    #log the errors
+##    except:
+##        print 'Error ! Unexpected value returned'
+##        
         
 
 #Launch a New Sandbox Instance
-def launch_new_instance(instance_name, instance_specification_id):
+def launch_new_instance(instance_name, instance_specification_id, volume_name, volume_size, volume_type):
 
     try:
         
@@ -38,10 +38,13 @@ def launch_new_instance(instance_name, instance_specification_id):
         data = "{\"image_id\":\"" + hortonworks_image_id + "\"," + \
                "\"is_boot_from_volume\": false," + \
                "\"name\":\"" + instance_name + "\"," + \
-               "\"network_name\":\"" + network_id + "\"," + \
-               "\"security_group_name\":\"" + security_group + "\"," + \
-               "\"security_key_name\":\"" + security_key + "\"," + \
+               "\"network_name\":\"" + network_name + "\"," + \
+               "\"security_group_name\":\"" + security_group_name + "\"," + \
+               "\"security_key_name\":\"" + security_key_name + "\"," + \
                "\"specification_id\":\"" + str(instance_specification_id) + "\"," + \
+               "\"volume_name\":\"" + volume_name + "\"," + \
+               "\"volume_size\":\"" + str(volume_size) + "\"," + \
+               "\"volume_type\":\"" + volume_type + "\"," + \
                "\"zone\": \"nova\" }"
         print data + '\n'
         response = requests.post(request_url, headers = headers, data = data)
@@ -49,7 +52,7 @@ def launch_new_instance(instance_name, instance_specification_id):
         r = response.json()
 
         #complete this later
-        instance_id = r[][]
+        instance_id = r['instance']['id']
         return instance_id
 
     except:
@@ -62,18 +65,19 @@ def create_new_floating_ip():
 
     try:
         
-        request_url = base_url + '/floating_ips'
+        request_url = base_url + 'floating_ips'
+        print request_url
 
         #add network_id here
         data = "{\"network_id\":\"" + network_id + "\"," + \
-               "\"project_id\":\"" + project_id + "\"}"
+               "\"project_id\":\"" + project_id + "\" }"
         print data + '\n'
         response = requests.post(request_url, headers = headers, data = data)
         print response.text + '\n'
         r = response.json()
 
         #complete this later
-        floating_ip_id = r[][]
+        floating_ip_id = r['floating_ip']['id']
         return floating_ip_id
 
     except:
@@ -86,7 +90,8 @@ def add_floating_ip_to_instance(instance_id, floating_ip_id):
 
     try:
 
-        request_url = base_url + '/floating_ips' + floating_ip_id + '/action'
+        request_url = base_url + 'floating_ips/' + floating_ip_id + '/action'
+        print request_url
 
         #add network_id here
         data = "{\"action\": \"add\"," + \
@@ -99,8 +104,9 @@ def add_floating_ip_to_instance(instance_id, floating_ip_id):
         r = response.json()
 
         #return floating ip address to calling program
-        floating_ip_addr = r[][]
-        return floating_ip_addr
+        floating_ip_addr = r['add']['address']
+        instance_name = r['add']['instance_name']
+        return floating_ip_addr, instance_name
         
 
     except:
@@ -108,32 +114,33 @@ def add_floating_ip_to_instance(instance_id, floating_ip_id):
 
 
 
-#Create a new spindle volume for the VM (limit to spindle for now)
-def create_new_spindle_volume(volume_name, volume_size, volume_type):
-
-    try:
-
-        if (volume_type = 'ssd'):
-            volume_id = total_ssd_volume_id
-        else:
-            if (volume_type = 'spindle'):
-            volume_id = total_spindle_volume_id
-            else:
-                print 'Error in type of volume field
-
-        request_url = base_url + project_id + '/volumes'
-
-        data = "{\"name\":\"" + volume_name + "\"," + \
-               "\"size\":" + volume_size + "," + \
-               "\"type\":\"" + volume_type + "\"," + \
-               "\"volume_id\":\"" + volume_id + "\"," + \
-               "\"zone\": \"nova\"}"
-        
-        print data + '\n'
-        response = requests.post(request_url, headers = headers, data = data)
-        print response.text + '\n'
-        r = response.json()
-
-    except:
-        print 'Error ! Unexpected value returned'
+###Create a new spindle volume for the VM (limit to spindle for now)
+##def create_new_spindle_volume(volume_name, volume_size, volume_type):
+##
+##    try:
+##
+##        if (volume_type = 'ssd'):
+##            volume_id = total_ssd_volume_id
+##        else:
+##            if (volume_type = 'spindle'):
+##            volume_id = total_spindle_volume_id
+##            else:
+##                print 'Error in type of volume field
+##
+##        request_url = base_url + project_id + '/volumes'
+##
+##        data = "{\"name\":\"" + volume_name + "\"," + \
+##               "\"size\":" + volume_size + "," + \
+##               "\"type\":\"" + volume_type + "\"," + \
+##               "\"volume_id\":\"" + volume_id + "\"," + \
+##               "\"zone\": \"nova\"}"
+##        
+##        print data + '\n'
+##        response = requests.post(request_url, headers = headers, data = data)
+##        print response.text + '\n'
+##        r = response.json()
+##        
+##
+##    except:
+##        print 'Error ! Unexpected value returned'
 
--- rest_api_initial.py
+++ rest_api_initial.py
@@ -1,19 +1,19 @@
-import auto_spin_up_api_calls
+from auto_spin_up_api_calls import *
 
 
 #test inputs
-cpus = 4
-disk = 50
-name = 'custom'
-ram = 8192
+##cpus = 4
+##disk = 50
+##name = 'custom'
+##ram = 8192
 instance_name = 'testinstance'
+instance_specification_id = 4
+volume_name = 'Boot_Volume_1'
+volume_size = 60
+volume_type = 'spindle'
 
-instance_specification_id = create_new_instance_specification(cpus, disk, name, ram)
-instance_id = launch_new_instance(instance_name, instance_id)
+##instance_specification_id = create_new_instance_specification(cpus, disk, name, ram)
+instance_id = launch_new_instance(instance_name, instance_specification_id, volume_name, volume_size, volume_type)
 floating_ip_id = create_new_floating_ip()
-floating_ip_addr = add_floating_ip_to_instance(instance_id, floating_ip_id)
-vm_volume_id = create_new_spindle_volume(volume_name, volume_size, volume_type)
-add_volume_to_vm(instance_id, vm_volume_id)
-
-
+floating_ip_addr, instance = add_floating_ip_to_instance(instance_id, floating_ip_id)
 
